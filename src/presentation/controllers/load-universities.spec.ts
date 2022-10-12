12a5f6b69f78550a9db176144fcfa98ecf2a8399
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 21:30:48 -0300

    test: ensure LoadUniversitiesController is defined

---
+++ src/presentation/controllers/load-universities.spec.ts
@@ -0,0 +1,7 @@
+import { LoadUniversitiesController } from '@/presentation/controllers/load-universities'
+
+describe('LoadUniversities Controller', () => {
+  it('should be defined', () => {
+    expect(new LoadUniversitiesController()).toBeDefined()
+  })
+})
