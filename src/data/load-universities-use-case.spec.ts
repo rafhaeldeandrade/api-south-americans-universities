ebc31faa13f0b231c02acac37a9feb0d0a9d71b9
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 22:33:02 -0300

    test: ensure LoadUniversitiesUseCase is defined

---
+++ src/data/load-universities-use-case.spec.ts
@@ -0,0 +1,7 @@
+import { LoadUniversities } from '@/data/load-universities-use-case'
+
+describe('LoadUniversitiesUseCase', () => {
+  it('should be defined', () => {
+    expect(new LoadUniversities()).toBeDefined()
+  })
+})
