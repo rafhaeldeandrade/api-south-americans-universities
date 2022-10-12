520fca7ee9e0cb84ebd855d41fcd7d7340f414a3
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 18:11:35 -0300

    test: ensure AddUniversity use case calls loadUniversityByProps.load with the correct values

--- src/data/add-university-use-case.spec.ts
+++ src/data/add-university-use-case.spec.ts
@@ -1,12 +1,52 @@
 import { AddUniversity } from '@/data/add-university-use-case'
+import { AddUniversityUseCaseInput } from '@/domain/contracts'
+import { faker } from '@faker-js/faker'
+import {
+  LoadUniversityByPropsRepository,
+  LoadUniversityBypropsRepositoryInput,
+  LoadUniversityBypropsRepositoryOutput
+} from '@/data/contracts'
+
+const LoadUniversityByPropsRepositoryStubReturn = {
+  id: faker.datatype.uuid(),
+  name: faker.lorem.words(),
+  domains: [faker.internet.domainName()],
+  country: faker.address.country(),
+  stateProvince: faker.address.state(),
+  alphaTwoCode: faker.address.countryCode(),
+  webPages: [faker.internet.url()]
+}
+class LoadUniversityByPropsRepositoryStub
+  implements LoadUniversityByPropsRepository
+{
+  async load(
+    university: LoadUniversityBypropsRepositoryInput
+  ): Promise<LoadUniversityBypropsRepositoryOutput> {
+    return LoadUniversityByPropsRepositoryStubReturn
+  }
+}
 
 interface SutTypes {
   sut: AddUniversity
+  loadUniversityByPropsRepositoryStub: LoadUniversityByPropsRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new AddUniversity()
-  return { sut }
+  const loadUniversityByPropsRepositoryStub =
+    new LoadUniversityByPropsRepositoryStub()
+  const sut = new AddUniversity(loadUniversityByPropsRepositoryStub)
+  return { sut, loadUniversityByPropsRepositoryStub }
+}
+
+function mockProps(): AddUniversityUseCaseInput {
+  return {
+    name: faker.lorem.words(),
+    domains: [faker.internet.domainName()],
+    country: faker.address.country(),
+    stateProvince: faker.address.state(),
+    alphaTwoCode: faker.address.countryCode(),
+    webPages: [faker.internet.url()]
+  }
 }
 
 describe('AddUniversity use case', () => {
@@ -18,5 +58,18 @@ describe('AddUniversity use case', () => {
   it('should have a method named add', async () => {
     const { sut } = makeSut()
     expect(sut.add).toBeDefined()
+  })
+
+  it('should call loadUniversityByProps.load with the correct values', async () => {
+    const { sut, loadUniversityByPropsRepositoryStub } = makeSut()
+    const props = mockProps()
+    const loadSpy = jest.spyOn(loadUniversityByPropsRepositoryStub, 'load')
+    await sut.add(props)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith({
+      country: props.country,
+      stateProvince: props.stateProvince,
+      name: props.name
+    })
   })
 })
