812582ec339c088c62c6e319d02b5b75bcd4cdff
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 18:49:18 -0300

    feat: implement ZodSchemaValidator

---
+++ src/presentation/helpers/zod-schema-validator.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+import { SchemaValidator } from '@/presentation/contracts'
+import { InvalidParamError } from '@/presentation/errors'
+import { MissingParamError } from '@/presentation/errors'
+
+export class ZodSchemaValidation implements SchemaValidator {
+  constructor(private readonly schema: z.ZodTypeAny) {}
+
+  async validate(input: any): Promise<Error | null> {
+    const result = await this.schema.safeParseAsync(input)
+    if (!result.success) {
+      const issueCode = result.error.issues[0].code
+      const issueMessage = result.error.issues[0].message
+      const invalidParam = result.error.issues[0].path[0].toString()
+      const invalidParams = [
+        'invalid_type',
+        'invalid_string',
+        'invalid_date',
+        'too_small',
+        'too_big'
+      ]
+      if (invalidParams.includes(issueCode)) {
+        if (issueMessage === 'Required') {
+          return new MissingParamError(invalidParam)
+        }
+        return new InvalidParamError(invalidParam)
+      }
+    }
+    return null
+  }
+}
