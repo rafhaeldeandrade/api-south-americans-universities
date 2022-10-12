1f8979bdc483ce0c1369336efdea8f0f22ee6504
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 22:43:22 -0300

    test: ensure LoadUniversitiesUseCase calls loadUniversitiesRepository with correct values

--- src/data/load-universities-use-case.spec.ts
+++ src/data/load-universities-use-case.spec.ts
@@ -1,11 +1,50 @@
 import { LoadUniversities } from '@/data/load-universities-use-case'
+import {
+  LoadUniversitiesRepository,
+  LoadUniversitiesRepositoryInput,
+  LoadUniversitiesRepositoryOutput
+} from '@/data/contracts'
+import { LoadUniversitiesUseCaseInput } from '@/domain/contracts'
+
+class LoadUniversitiesRepositoryStub implements LoadUniversitiesRepository {
+  async load(
+    props: LoadUniversitiesRepositoryInput
+  ): Promise<LoadUniversitiesRepositoryOutput> {
+    return []
+  }
+}
+
+interface SutTypes {
+  sut: LoadUniversities
+  loadUniversitiesRepositoryStub: LoadUniversitiesRepository
+}
+
+function makeSut(): SutTypes {
+  const loadUniversitiesRepositoryStub = new LoadUniversitiesRepositoryStub()
+  const sut = new LoadUniversities(loadUniversitiesRepositoryStub)
+  return {
+    sut,
+    loadUniversitiesRepositoryStub
+  }
+}
 
 describe('LoadUniversitiesUseCase', () => {
   it('should be defined', () => {
-    expect(new LoadUniversities()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
   })
 
   it('should have a method named load', () => {
-    expect(new LoadUniversities().load).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.load).toBeDefined()
+  })
+
+  it('should call loadUniversitiesRepository with correct values', async () => {
+    const { sut, loadUniversitiesRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadUniversitiesRepositoryStub, 'load')
+    const props = {} as LoadUniversitiesUseCaseInput
+    await sut.load(props)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(props)
   })
 })
