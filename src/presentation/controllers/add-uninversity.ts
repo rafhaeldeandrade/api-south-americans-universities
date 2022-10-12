a857ed9d6824c735a2e7f47a233687e8bc34c6f8
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 16:30:24 -0300

    feat: ensure AddUniversityController returns 400 if schemaValidator.validate returns an error

--- src/presentation/controllers/add-uninversity.ts
+++ src/presentation/controllers/add-uninversity.ts
@@ -1,10 +1,12 @@
 import { Controller, SchemaValidator } from '@/presentation/contracts'
+import { badRequest } from '../helpers/http-helper'
 
 export class AddUniversityController implements Controller {
   constructor(private readonly schemaValidator: SchemaValidator) {}
 
   async handle(request: any): Promise<any> {
-    await this.schemaValidator.validate(request.body)
+    const error = await this.schemaValidator.validate(request.body)
+    if (error) return badRequest(error)
     return null
   }
 }
