a07419e7ac50feb9588c4543a22b5907ce638cd9
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 22:33:35 -0300

    test: ensure LoadUniversitiesUseCase has a method named load

--- src/data/load-universities-use-case.spec.ts
+++ src/data/load-universities-use-case.spec.ts
@@ -4,4 +4,8 @@ describe('LoadUniversitiesUseCase', () => {
   it('should be defined', () => {
     expect(new LoadUniversities()).toBeDefined()
   })
+
+  it('should have a method named load', () => {
+    expect(new LoadUniversities().load).toBeDefined()
+  })
 })
