f2243b5a401c8f52a61cd3f3a097f74f8480c8c0
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 21:15:46 -0300

    feat: add populate database script

---
+++ src/main/populate-database.ts
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+import env from '@/main/config/env'
+import { University } from '@/domain/contracts'
+import { UniversityModel } from '@/infra/mongo/schemas/university'
+import { mongooseHelper } from '@/infra/mongo/helpers/mongoose-helper'
+
+function mapToModel(universities: any[]): University[] {
+  return universities.flatMap((university) => ({
+    stateProvince: university['state-province'],
+    alphaTwoCode: university['alpha_two_code'],
+    webPages: university['web_pages'],
+    country: university['country'],
+    name: university['name'],
+    domains: university['domains']
+  }))
+}
+
+async function populateDatabase() {
+  const countries = [
+    'argentina',
+    'brasil',
+    'chile',
+    'colombia',
+    'paraguai',
+    'peru',
+    'suriname',
+    'uruguay'
+  ]
+  const universities = await Promise.all(
+    countries.map((country) => fetchUniversities(country))
+  )
+  const universitiesToSave = mapToModel(universities.flat())
+  await UniversityModel.insertMany(universitiesToSave)
+}
+
+async function fetchUniversities(country: string): Promise<void> {
+  const { data } = await axios.get(
+    `http://universities.hipolabs.com/search?country=${country}`
+  )
+  return data
+}
+
+mongooseHelper
+  .connect(`${env.mongoUrl}/universities`)
+  .then(async () => {
+    await populateDatabase()
+  })
+  .finally(async () => await mongooseHelper.disconnect())
