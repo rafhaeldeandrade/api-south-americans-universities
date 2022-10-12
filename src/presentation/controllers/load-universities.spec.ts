b809210a62fe049dee02ca31d07eea4e996a6c3b
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 22:12:56 -0300

    test: ensure LoadUniversitiesController calls loadUniversitiesUseCase with correct values

--- src/presentation/controllers/load-universities.spec.ts
+++ src/presentation/controllers/load-universities.spec.ts
@@ -2,17 +2,30 @@ import { faker } from '@faker-js/faker'
 import { LoadUniversitiesController } from '@/presentation/controllers/load-universities'
 import { InvalidParamError } from '@/presentation/errors'
 import { badRequest } from '../helpers/http-helper'
+import { LoadUniversitiesUseCase } from '@/domain/contracts'
+
+class LoadUniversitiesUseCaseStub implements LoadUniversitiesUseCase {
+  async load() {
+    return []
+  }
+}
 
 interface SutTypes {
   sut: LoadUniversitiesController
+  loadUniversitiesUseCaseStub: LoadUniversitiesUseCase
 }
 
 function makeSut(): SutTypes {
-  const sut = new LoadUniversitiesController()
-  return { sut }
+  const loadUniversitiesUseCaseStub = new LoadUniversitiesUseCaseStub()
+  const sut = new LoadUniversitiesController(loadUniversitiesUseCaseStub)
+  return { sut, loadUniversitiesUseCaseStub }
 }
 
 describe('LoadUniversities Controller', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
   it('should be defined', () => {
     const { sut } = makeSut()
     expect(sut).toBeDefined()
@@ -23,16 +36,33 @@ describe('LoadUniversities Controller', () => {
     expect(sut.handle).toBeDefined()
   })
 
-  it('should return 400 if query.page is provided but it is not a number', () => {
+  it('should return 400 if query.page is provided but it is not a number', async () => {
     const { sut } = makeSut()
     const fakeHttpRequest = {
       query: {
         page: faker.random.word()
       }
     }
-    const response = sut.handle(fakeHttpRequest)
+    const response = await sut.handle(fakeHttpRequest)
     expect(response).toEqual(
       badRequest(new InvalidParamError('Page must be a number'))
     )
+  })
+
+  it('should call loadUniversitiesUseCase with correct values', async () => {
+    const { sut, loadUniversitiesUseCaseStub } = makeSut()
+    const loadSpy = jest.spyOn(loadUniversitiesUseCaseStub, 'load')
+    const fakeHttpRequest = {
+      query: {
+        page: faker.datatype.number(20),
+        country: faker.address.country()
+      }
+    }
+    await sut.handle(fakeHttpRequest)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith({
+      page: fakeHttpRequest.query.page,
+      country: fakeHttpRequest.query.country
+    })
   })
 })
