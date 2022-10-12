cb46bb7c84dabac12100d79579bb040e5aefd72e
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 10:57:31 -0300

    feat: ensure LoadUniversityById use case returns the correct values on success

--- src/data/load-university-by-id-use-case.ts
+++ src/data/load-university-by-id-use-case.ts
@@ -13,15 +13,9 @@ export class LoadUniversityById implements LoadUniversityByIdUseCase {
   async load(
     props: LoadUniversityByIdUseCaseInput
   ): Promise<LoadUniversityByIdUseCaseOutput> {
-    await this.loadUniversityByIdRepository.load(props.universityId)
-    return {
-      id: '1',
-      name: 'University of California, Berkeley',
-      country: 'United States',
-      stateProvince: 'California',
-      domains: ['berkeley.edu'],
-      webPages: ['http://www.berkeley.edu/'],
-      alphaTwoCode: 'US'
-    }
+    const university = await this.loadUniversityByIdRepository.load(
+      props.universityId
+    )
+    return university
   }
 }
