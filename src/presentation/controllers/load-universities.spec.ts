e37fd9bd944b7b8ce0bc1126c99f4a7adc30c593
Rafhael Gomes <rafhael@protonmail.com>
2022-10-11 21:59:02 -0300

    test: ensure LoadUniversitiesController returns 400 if query.page is provided but it is not a number

--- src/presentation/controllers/load-universities.spec.ts
+++ src/presentation/controllers/load-universities.spec.ts
@@ -1,11 +1,38 @@
+import { faker } from '@faker-js/faker'
 import { LoadUniversitiesController } from '@/presentation/controllers/load-universities'
+import { InvalidParamError } from '@/presentation/errors'
+import { badRequest } from '../helpers/http-helper'
+
+interface SutTypes {
+  sut: LoadUniversitiesController
+}
+
+function makeSut(): SutTypes {
+  const sut = new LoadUniversitiesController()
+  return { sut }
+}
 
 describe('LoadUniversities Controller', () => {
   it('should be defined', () => {
-    expect(new LoadUniversitiesController()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
   })
 
   it('should have a method named handle', () => {
-    expect(new LoadUniversitiesController().handle).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.handle).toBeDefined()
+  })
+
+  it('should return 400 if query.page is provided but it is not a number', () => {
+    const { sut } = makeSut()
+    const fakeHttpRequest = {
+      query: {
+        page: faker.random.word()
+      }
+    }
+    const response = sut.handle(fakeHttpRequest)
+    expect(response).toEqual(
+      badRequest(new InvalidParamError('Page must be a number'))
+    )
   })
 })
