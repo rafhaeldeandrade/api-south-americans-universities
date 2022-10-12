99198ba07041a219ee8c1a37a7d0c48b0bd68c9d
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 19:11:38 -0300

    feat: implement POST /api/universities route

--- src/main/config/express/university.routes.ts
+++ src/main/config/express/university.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { makeLoadUniversitiesController } from '@/main/factories/make-load-universities'
 import { adaptRoute } from '@/main/config/express/adapt-routes'
 import { makeLoadUniversityByIdController } from '@/main/factories/make-load-university-by-id'
+import { makeAddUniversityController } from '@/main/factories/make-add-university'
 
 export const universityRouter = Router()
 
@@ -10,3 +11,4 @@ universityRouter.get(
   adaptRoute(makeLoadUniversityByIdController())
 )
 universityRouter.get('/', adaptRoute(makeLoadUniversitiesController()))
+universityRouter.post('/', adaptRoute(makeAddUniversityController()))
---
+++ src/main/factories/make-add-university.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+import { Controller } from '@/presentation/contracts'
+import { ZodSchemaValidator } from '@/presentation/helpers/zod-schema-validator'
+import { MongoLoadUniversityByProps } from '@/infra/mongo/load-university-by-props'
+import { MongoAddUniversity } from '@/infra/mongo/add-university'
+import { AddUniversity } from '@/data/add-university-use-case'
+import { AddUniversityController } from '@/presentation/controllers/add-university'
+
+export function makeAddUniversityController(): Controller {
+  const zodSchema = z.object({
+    name: z.string().min(5).max(100),
+    country: z.string().min(5).max(25),
+    stateProvince: z.string().min(5).max(25).nullable(),
+    domains: z.array(z.string().min(5).max(100)),
+    webPages: z.array(z.string().min(5).max(100).url()),
+    alphaTwoCode: z.string().min(2).max(2)
+  })
+  const zodSchemaValidator = new ZodSchemaValidator(zodSchema)
+  const loadUniversityByProps = new MongoLoadUniversityByProps()
+  const addUniversity = new MongoAddUniversity()
+  const useCase = new AddUniversity(loadUniversityByProps, addUniversity)
+  return new AddUniversityController(zodSchemaValidator, useCase)
+}
