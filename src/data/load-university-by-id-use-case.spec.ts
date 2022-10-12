094556ea27030f68cd94f2b4a981ee817cf250d8
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 10:53:35 -0300

    test: ensure LoadUniversityById use case calls loadUniversityByIdRepository with the correct value

--- src/data/load-university-by-id-use-case.spec.ts
+++ src/data/load-university-by-id-use-case.spec.ts
@@ -1,12 +1,38 @@
 import { LoadUniversityById } from '@/data/load-university-by-id-use-case'
+import { faker } from '@faker-js/faker'
+import {
+  LoadUniversityByIdRepository,
+  LoadUniversityByIdRepositoryInput,
+  LoadUniversityByIdRepositoryOutput
+} from '@/data/contracts'
+
+const LoadUniversityByIdRepositoryStubReturn = {
+  id: faker.datatype.uuid(),
+  name: faker.name.fullName(),
+  country: faker.address.country(),
+  stateProvince: faker.address.state(),
+  domains: [faker.internet.domainName()],
+  webPages: [faker.internet.url()],
+  alphaTwoCode: faker.address.countryCode()
+}
+class LoadUniversityByIdRepositoryStub implements LoadUniversityByIdRepository {
+  async load(
+    id: LoadUniversityByIdRepositoryInput
+  ): Promise<LoadUniversityByIdRepositoryOutput> {
+    return LoadUniversityByIdRepositoryStubReturn
+  }
+}
 
 interface SutTypes {
   sut: LoadUniversityById
+  loadUniversityByIdRepositoryStub: LoadUniversityByIdRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new LoadUniversityById()
-  return { sut }
+  const loadUniversityByIdRepositoryStub =
+    new LoadUniversityByIdRepositoryStub()
+  const sut = new LoadUniversityById(loadUniversityByIdRepositoryStub)
+  return { sut, loadUniversityByIdRepositoryStub }
 }
 
 describe('LoadUniversityById use case', () => {
@@ -18,5 +44,16 @@ describe('LoadUniversityById use case', () => {
   it('should have a method named load', () => {
     const { sut } = makeSut()
     expect(sut.load).toBeDefined()
+  })
+
+  it('should call loadUniversityByIdRepository with the correct value', async () => {
+    const { sut, loadUniversityByIdRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadUniversityByIdRepositoryStub, 'load')
+    const props = {
+      universityId: faker.datatype.uuid()
+    }
+    await sut.load(props)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(props.universityId)
   })
 })
