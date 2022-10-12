1568a71845715d32faf88ccf46f704afdb3f2e44
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 16:27:07 -0300

    test: ensure AddUniversityController calls schemaValidator with the correct values

--- src/presentation/controllers/add-university.spec.ts
+++ src/presentation/controllers/add-university.spec.ts
@@ -1,13 +1,37 @@
 import { AddUniversityController } from '@/presentation/controllers/add-uninversity'
+import { HttpRequest, SchemaValidator } from '@/presentation/contracts'
+import { faker } from '@faker-js/faker'
+
+class SchemaValidationStub implements SchemaValidator {
+  async validate(input: any): Promise<Error | null> {
+    return null
+  }
+}
 
 interface SutTypes {
   sut: AddUniversityController
+  schemaValidationStub: SchemaValidator
 }
 
 function makeSut(): SutTypes {
-  const sut = new AddUniversityController()
+  const schemaValidationStub = new SchemaValidationStub()
+  const sut = new AddUniversityController(schemaValidationStub)
   return {
-    sut
+    sut,
+    schemaValidationStub
+  }
+}
+
+function mockRequest(): HttpRequest {
+  return {
+    body: {
+      name: faker.lorem.words(),
+      domains: [faker.internet.domainName()],
+      country: faker.address.country(),
+      stateProvince: faker.address.state(),
+      alphaTwoCode: faker.address.countryCode(),
+      webPages: [faker.internet.url()]
+    }
   }
 }
 
@@ -20,5 +44,14 @@ describe('AddUniversity Controller', () => {
   it('should have a method named handle', () => {
     const { sut } = makeSut()
     expect(sut.handle).toBeDefined()
+  })
+
+  it('should call schemaValidator.validate with the correct values', async () => {
+    const { sut, schemaValidationStub } = makeSut()
+    const httpRequest = mockRequest()
+    const validateSpy = jest.spyOn(schemaValidationStub, 'validate')
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
