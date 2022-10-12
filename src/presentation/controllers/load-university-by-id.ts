b1843ced088089a461a679e5f6af5d57fa29b9d4
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 10:07:01 -0300

    feat: ensure LoadUniversityByIdController is defined

---
+++ src/presentation/controllers/load-university-by-id.ts
@@ -0,0 +1,7 @@
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/contracts'
+
+export class LoadUniversityByIdController implements Controller {
+  async handle(props: HttpRequest): Promise<HttpResponse> {
+    return null as unknown as HttpResponse
+  }
+}
