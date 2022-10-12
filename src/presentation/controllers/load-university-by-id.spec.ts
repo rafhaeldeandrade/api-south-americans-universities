176dcc96d2dd0667faf197a4bcffcd2fa774e17b
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 10:08:34 -0300

    feat: ensure LoadUniversityByIdController has a method named handle

--- src/presentation/controllers/load-university-by-id.spec.ts
+++ src/presentation/controllers/load-university-by-id.spec.ts
@@ -1,7 +1,24 @@
 import { LoadUniversityByIdController } from '@/presentation/controllers/load-university-by-id'
 
+interface SutTypes {
+  sut: LoadUniversityByIdController
+}
+
+function makeSut(): SutTypes {
+  const sut = new LoadUniversityByIdController()
+  return {
+    sut
+  }
+}
+
 describe('LoadUniversityById Controller', () => {
   it('should be defined', () => {
-    expect(new LoadUniversityByIdController()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
+  })
+
+  it('should have a method named handle', () => {
+    const { sut } = makeSut()
+    expect(sut.handle).toBeDefined()
   })
 })
