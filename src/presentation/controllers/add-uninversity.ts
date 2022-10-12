5a12e25642e3880d8cc80e72e4b18ecec5b04924
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 16:32:53 -0300

    feat: ensure AddUniversityController returns 500 if schemaValidator.validate throws

--- src/presentation/controllers/add-uninversity.ts
+++ src/presentation/controllers/add-uninversity.ts
@@ -1,12 +1,16 @@
 import { Controller, SchemaValidator } from '@/presentation/contracts'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, internalServerError } from '../helpers/http-helper'
 
 export class AddUniversityController implements Controller {
   constructor(private readonly schemaValidator: SchemaValidator) {}
 
   async handle(request: any): Promise<any> {
-    const error = await this.schemaValidator.validate(request.body)
-    if (error) return badRequest(error)
-    return null
+    try {
+      const error = await this.schemaValidator.validate(request.body)
+      if (error) return badRequest(error)
+      return null
+    } catch (e) {
+      return internalServerError()
+    }
   }
 }
