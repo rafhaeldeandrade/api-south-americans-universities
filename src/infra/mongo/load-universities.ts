cad08708612222ef87c1e5e9c1aafe67535fc2ce
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 00:04:22 -0300

    feat: implement MongoLoadUniversities repository

---
+++ src/infra/mongo/load-universities.ts
@@ -0,0 +1,30 @@
+import {
+  LoadUniversitiesRepository,
+  LoadUniversitiesRepositoryInput,
+  LoadUniversitiesRepositoryOutput
+} from '@/data/contracts'
+import { UniversityModel } from '@/infra/mongo/schemas/university'
+
+export class MongoLoadUniversities implements LoadUniversitiesRepository {
+  async load(
+    props: LoadUniversitiesRepositoryInput
+  ): Promise<LoadUniversitiesRepositoryOutput> {
+    const { country, skip, limit } = props
+    const filters = {} as any
+    if (country) filters['country'] = country
+    const universities = await UniversityModel.find(
+      filters,
+      {},
+      { lean: true, skip, limit }
+    )
+    return universities.map((university) => ({
+      id: university._id.toString(),
+      name: university.name,
+      country: university.country,
+      domains: university.domains,
+      webPages: university.webPages,
+      alphaTwoCode: university.alphaTwoCode,
+      stateProvince: university.stateProvince
+    }))
+  }
+}
