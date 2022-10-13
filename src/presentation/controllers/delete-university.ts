d0795e5dd9aa2de6cc265cac6c1fdf4f88a7df66
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 22:56:04 -0300

    feat: ensure DeleteUniversityController returns 500 if schemaValidator.validate throws

--- src/presentation/controllers/delete-university.ts
+++ src/presentation/controllers/delete-university.ts
@@ -4,14 +4,19 @@ import {
   HttpResponse,
   SchemaValidator
 } from '@/presentation/contracts'
+import { internalServerError } from '@/presentation/helpers/http-helper'
 
 export class DeleteUniversityController implements Controller {
   constructor(private readonly schemaValidator: SchemaValidator) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    await this.schemaValidator.validate({
-      universityId: request?.params?.universityId
-    })
-    return null as unknown as HttpResponse
+    try {
+      await this.schemaValidator.validate({
+        universityId: request?.params?.universityId
+      })
+      return null as unknown as HttpResponse
+    } catch (e) {
+      return internalServerError()
+    }
   }
 }
