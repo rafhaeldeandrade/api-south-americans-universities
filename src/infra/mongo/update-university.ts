9a4536b2aae485f7452da572e1f674427d768bdc
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 23:48:32 -0300

    refactor: return null on error

--- src/infra/mongo/update-university.ts
+++ src/infra/mongo/update-university.ts
@@ -9,24 +9,28 @@ export class MongoUpdateUniversity implements UpdateUniversityRepository {
   async update(
     props: UpdateUniversityRepositoryInput
   ): Promise<UpdateUniversityRepositoryOutput> {
-    const { id, name, domains, webPages } = props
-    const university = await UniversityModel.findOneAndUpdate(
-      { _id: id },
-      { name, domains, webPages },
-      {
-        lean: true,
-        new: true
+    try {
+      const { id, name, domains, webPages } = props
+      const university = await UniversityModel.findOneAndUpdate(
+        { _id: id },
+        { name, domains, webPages },
+        {
+          lean: true,
+          new: true
+        }
+      )
+      if (!university) return null
+      return {
+        id: university._id.toString(),
+        name: university.name,
+        domains: university.domains,
+        webPages: university.webPages,
+        country: university.country,
+        stateProvince: university.stateProvince,
+        alphaTwoCode: university.alphaTwoCode
       }
-    )
-    if (!university) return null
-    return {
-      id: university._id.toString(),
-      name: university.name,
-      domains: university.domains,
-      webPages: university.webPages,
-      country: university.country,
-      stateProvince: university.stateProvince,
-      alphaTwoCode: university.alphaTwoCode
+    } catch (e) {
+      return null
     }
   }
 }
