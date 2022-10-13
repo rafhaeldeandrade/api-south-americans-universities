fe6cd2c4c5ed86664e855daa6ac62454765ed68d
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:16:03 -0300

    feat: ensure UpdateUniversityController calls schemaValidator.validate with the correct values

--- src/presentation/controllers/update-university.ts
+++ src/presentation/controllers/update-university.ts
@@ -1,7 +1,20 @@
-import { Controller, HttpRequest, HttpResponse } from '../contracts'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  SchemaValidator
+} from '@/presentation/contracts'
 
 export class UpdateUniversityController implements Controller {
+  constructor(private readonly schemaValidator: SchemaValidator) {}
+
   async handle(request: HttpRequest): Promise<HttpResponse> {
+    await this.schemaValidator.validate({
+      webPages: request?.body?.webPages,
+      name: request?.body?.name,
+      domains: request?.body?.domains,
+      universityId: request?.params?.universityId
+    })
     return null as unknown as HttpResponse
   }
 }
