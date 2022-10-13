96c42f75641e452241185c4f43609064f04b61a6
Rafhael Gomes <rafhael@protonmail.com>
2022-10-13 00:45:06 -0300

    refactor: use zodSchema in loadUniversityById factory function

--- src/main/factories/make-load-university-by-id.ts
+++ src/main/factories/make-load-university-by-id.ts
@@ -1,10 +1,17 @@
+import { z } from 'zod'
+
 import { Controller } from '@/presentation/contracts'
 import { MongoLoadUniversityById } from '@/infra/mongo/load-university-by-id'
 import { LoadUniversityById } from '@/data/load-university-by-id-use-case'
 import { LoadUniversityByIdController } from '@/presentation/controllers/load-university-by-id'
+import { ZodSchemaValidator } from '@/presentation/helpers/zod-schema-validator'
 
 export function makeLoadUniversityByIdController(): Controller {
+  const zodSchema = z.object({
+    universityId: z.string()
+  })
+  const schemaValidator = new ZodSchemaValidator(zodSchema)
   const loadUniversityByIdRepository = new MongoLoadUniversityById()
   const useCase = new LoadUniversityById(loadUniversityByIdRepository)
-  return new LoadUniversityByIdController(useCase)
+  return new LoadUniversityByIdController(schemaValidator, useCase)
 }
