06f4ce53f4e40f3514a337dd337b88ad6b13a059
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:53:00 -0300

    feat: ensure UpdateUniversity use case calls loadUniversityById.load with the correct values

--- src/data/update-university-use-case.ts
+++ src/data/update-university-use-case.ts
@@ -3,11 +3,18 @@ import {
   UpdateUniversityUseCaseInput,
   UpdateUniversityUseCaseOutput
 } from '@/domain/contracts'
+import { LoadUniversityByIdRepository } from './contracts'
 
 export class UpdateUniversity implements UpdateUniversityUseCase {
+  constructor(
+    private readonly loadUniversityByIdRepository: LoadUniversityByIdRepository
+  ) {}
+
   async update(
     props: UpdateUniversityUseCaseInput
   ): Promise<UpdateUniversityUseCaseOutput> {
+    const { universityId } = props
+    await this.loadUniversityByIdRepository.load(universityId)
     return null
   }
 }
