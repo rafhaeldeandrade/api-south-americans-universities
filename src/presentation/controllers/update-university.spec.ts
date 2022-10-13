defd2b5a60d214104febee8455e7352566f70944
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:16:15 -0300

    test: ensure UpdateUniversityController calls schemaValidator.validate with the correct values

--- src/presentation/controllers/update-university.spec.ts
+++ src/presentation/controllers/update-university.spec.ts
@@ -1,13 +1,37 @@
 import { UpdateUniversityController } from '@/presentation/controllers/update-university'
+import { faker } from '@faker-js/faker'
+import { SchemaValidator } from '@/presentation/contracts'
+
+class SchemaValidationStub implements SchemaValidator {
+  async validate(input: any): Promise<Error | null> {
+    return null
+  }
+}
 
 interface SutTypes {
   sut: UpdateUniversityController
+  schemaValidatorStub: SchemaValidator
 }
 
 function makeSut(): SutTypes {
-  const sut = new UpdateUniversityController()
+  const schemaValidatorStub = new SchemaValidationStub()
+  const sut = new UpdateUniversityController(schemaValidatorStub)
   return {
-    sut
+    sut,
+    schemaValidatorStub
+  }
+}
+
+function mockHttpRequest() {
+  return {
+    params: {
+      universityId: faker.datatype.uuid()
+    },
+    body: {
+      webPages: [faker.internet.url()],
+      name: faker.company.name(),
+      domains: [faker.internet.domainName()]
+    }
   }
 }
 
@@ -20,5 +44,19 @@ describe('UpdateUniversityController', () => {
   it('should have a method named handle', () => {
     const { sut } = makeSut()
     expect(sut.handle).toBeDefined()
+  })
+
+  it('should call schemaValidator.validate with the correct values', () => {
+    const { sut, schemaValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(schemaValidatorStub, 'validate')
+    const httpRequest = mockHttpRequest()
+    sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith({
+      webPages: httpRequest.body.webPages,
+      name: httpRequest.body.name,
+      domains: httpRequest.body.domains,
+      universityId: httpRequest.params.universityId
+    })
   })
 })
