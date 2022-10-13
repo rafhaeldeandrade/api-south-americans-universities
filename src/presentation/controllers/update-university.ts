a23df327b42bc983219be4d09fe8e9c0a815e3ed
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:09:05 -0300

    feat: ensure UpdateUniversityController is defined

---
+++ src/presentation/controllers/update-university.ts
@@ -0,0 +1,7 @@
+import { Controller, HttpRequest, HttpResponse } from '../contracts'
+
+export class UpdateUniversityController implements Controller {
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    return null as unknown as HttpResponse
+  }
+}
