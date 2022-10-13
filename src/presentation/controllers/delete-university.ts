21b7fe586c27777197e2aa84478cacc5568a759d
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 22:54:46 -0300

    feat: ensure DeleteUniversityController calls schemaValidator.validate with the correct values

--- src/presentation/controllers/delete-university.ts
+++ src/presentation/controllers/delete-university.ts
@@ -1,7 +1,17 @@
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/contracts'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  SchemaValidator
+} from '@/presentation/contracts'
 
 export class DeleteUniversityController implements Controller {
+  constructor(private readonly schemaValidator: SchemaValidator) {}
+
   async handle(request: HttpRequest): Promise<HttpResponse> {
+    await this.schemaValidator.validate({
+      universityId: request?.params?.universityId
+    })
     return null as unknown as HttpResponse
   }
 }
