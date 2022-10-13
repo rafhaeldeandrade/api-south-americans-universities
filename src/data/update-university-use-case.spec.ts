a19fa8bfc339d269d512543f56b7cffd28a86f16
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:53:11 -0300

    test: ensure UpdateUniversity use case calls loadUniversityById.load with the correct values

--- src/data/update-university-use-case.spec.ts
+++ src/data/update-university-use-case.spec.ts
@@ -1,13 +1,50 @@
+import { faker } from '@faker-js/faker'
+
 import { UpdateUniversity } from '@/data/update-university-use-case'
+import {
+  LoadUniversityByIdRepository,
+  LoadUniversityByIdRepositoryInput,
+  LoadUniversityByIdRepositoryOutput
+} from '@/data/contracts'
+
+const loadUniversityByIdRepositoryStubReturn = {
+  id: faker.datatype.uuid(),
+  name: faker.name.fullName(),
+  country: faker.address.country(),
+  stateProvince: faker.address.state(),
+  domains: [faker.internet.domainName()],
+  webPages: [faker.internet.url()],
+  alphaTwoCode: faker.address.countryCode()
+}
+class LoadUniversityByIdRepositoryStub implements LoadUniversityByIdRepository {
+  async load(
+    id: LoadUniversityByIdRepositoryInput
+  ): Promise<LoadUniversityByIdRepositoryOutput> {
+    return loadUniversityByIdRepositoryStubReturn
+  }
+}
 
 interface SutTypes {
   sut: UpdateUniversity
+  loadUniversityByIdRepositoryStub: LoadUniversityByIdRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new UpdateUniversity()
+  const loadUniversityByIdRepositoryStub =
+    new LoadUniversityByIdRepositoryStub()
+  const sut = new UpdateUniversity(loadUniversityByIdRepositoryStub)
   return {
-    sut
+    sut,
+    loadUniversityByIdRepositoryStub
+  }
+}
+
+function mockProps() {
+  return {
+    universityId: faker.datatype.uuid(),
+    name: faker.lorem.words(),
+    domains: [faker.internet.domainName()],
+    webPages: [faker.internet.url()]
   }
 }
 
@@ -20,5 +57,14 @@ describe('UpdateUniversity use case', () => {
   it('should have a method named update', async () => {
     const { sut } = makeSut()
     expect(sut.update).toBeDefined()
+  })
+
+  it('should call loadUniversityById.load with the correct values', async () => {
+    const { sut, loadUniversityByIdRepositoryStub } = makeSut()
+    const props = mockProps()
+    const loadSpy = jest.spyOn(loadUniversityByIdRepositoryStub, 'load')
+    await sut.update(props)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(props.universityId)
   })
 })
