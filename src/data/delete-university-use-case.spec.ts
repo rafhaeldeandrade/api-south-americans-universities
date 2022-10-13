f1168fca5be13daae05a114edd48493b7a2835cf
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 23:25:40 -0300

    test: ensure DeleteUniversity use case calls deleteUniversityRepository.delete with the correct values

--- src/data/delete-university-use-case.spec.ts
+++ src/data/delete-university-use-case.spec.ts
@@ -1,15 +1,35 @@
 import { faker } from '@faker-js/faker'
 import { DeleteUniversity } from '@/data/delete-university-use-case'
+import {
+  DeleteUniversityRepository,
+  DeleteUniversityRepositoryInput,
+  DeleteUniversityRepositoryOutput
+} from '@/data/contracts'
+
+class DeleteUniversityRepositoryStub implements DeleteUniversityRepository {
+  async delete(
+    props: DeleteUniversityRepositoryInput
+  ): Promise<DeleteUniversityRepositoryOutput> {
+    return Promise.resolve()
+  }
+}
 
 interface SutTypes {
   sut: DeleteUniversity
+  deleteUniversityRepositoryStub: DeleteUniversityRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new DeleteUniversity()
+  const deleteUniversityRepositoryStub = new DeleteUniversityRepositoryStub()
+  const sut = new DeleteUniversity(deleteUniversityRepositoryStub)
   return {
-    sut
+    sut,
+    deleteUniversityRepositoryStub
   }
+}
+
+function mockProps() {
+  return faker.datatype.uuid()
 }
 
 describe('UpdateUniversity use case', () => {
@@ -21,5 +41,14 @@ describe('UpdateUniversity use case', () => {
   it('should have a method named delete', async () => {
     const { sut } = makeSut()
     expect(sut.delete).toBeDefined()
+  })
+
+  it('should call deleteUniversityRepository.delete with the correct values', async () => {
+    const { sut, deleteUniversityRepositoryStub } = makeSut()
+    const props = mockProps()
+    const deleteSpy = jest.spyOn(deleteUniversityRepositoryStub, 'delete')
+    await sut.delete(props)
+    expect(deleteSpy).toHaveBeenCalledTimes(1)
+    expect(deleteSpy).toHaveBeenCalledWith(props)
   })
 })
