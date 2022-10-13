656f6e6a2d6b8bd858ac4a26e38dcbb9d24d1733
Rafhael Gomes <rafhael@protonmail.com>
2022-10-12 21:17:44 -0300

    feat: ensure UpdateUniversityController returns 400 if schemaValidator.validate returns an error

--- src/presentation/controllers/update-university.ts
+++ src/presentation/controllers/update-university.ts
@@ -4,17 +4,19 @@ import {
   HttpResponse,
   SchemaValidator
 } from '@/presentation/contracts'
+import { badRequest } from '../helpers/http-helper'
 
 export class UpdateUniversityController implements Controller {
   constructor(private readonly schemaValidator: SchemaValidator) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    await this.schemaValidator.validate({
+    const error = await this.schemaValidator.validate({
       webPages: request?.body?.webPages,
       name: request?.body?.name,
       domains: request?.body?.domains,
       universityId: request?.params?.universityId
     })
+    if (error) return badRequest(error)
     return null as unknown as HttpResponse
   }
 }
